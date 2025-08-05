73e0827d3d313f49ba2b536bc81cf3ddf780b8a6
bahn010 <byungmin.ahn@pwc.com>
2025-08-05 23:35:46 +0900

    백엔드 코드 수정

--- backend/routes/auth.api.js
+++ backend/routes/auth.api.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const authController = require("../controller/auth.controller")
+const userController = require("../controller/user.controller")
 
 router.post("/login", authController.loginWithEmail)
 router.get("/me", authController.authenticateToken, userController.getUser)
